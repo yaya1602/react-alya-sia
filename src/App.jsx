a335c50dd56ba6fa35ecfe4fa87dc2afbe9dbacc
yaya1602 <alya24si@mahasiswa.pcr.ac.id>
2026-04-28 09:36:27 +0700

    Login API + Axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,37 @@
 import { useState } from "react";
-import Dashboard from "./pages/Dashboard";
+// import Dashboard from "./pages/Dashboard";
 import "./assets/tailwind.css";
 import { Routes, Route } from "react-router-dom";
-import Customers from "./pages/Customer";
-import Orders from "./pages/Orders";
-import NotFound from "./pages/NotFound";
-import ErrorPage from "./pages/ErrorPage";
-import MainLayout from "./layouts/MainLayout";
-import AuthLayout from "./layouts/AuthLayout";
-import Login from "./pages/auth/Login";
-import Register from "./pages/auth/Register";
-import Forgot from "./pages/auth/Forgot";
+// import Customers from "./pages/Customer";
+// import Orders from "./pages/Orders";
+// import NotFound from "./pages/NotFound";
+// import ErrorPage from "./pages/ErrorPage";
+// import MainLayout from "./layouts/MainLayout";
+// import AuthLayout from "./layouts/AuthLayout";
+// import Login from "./pages/auth/Login";
+// import Register from "./pages/auth/Register";
+// import Forgot from "./pages/auth/Forgot";
+import { Suspense } from "react";
+import Loading from "./components/Loading";
+import React from "react";
 
 function App() {
+  const Dashboard = React.lazy(() => import("./pages/Dashboard"))
+  const Orders = React.lazy(() => import("./pages/Orders"))
+  const Customers = React.lazy(() => import("./pages/Customer"))
+  const NotFound = React.lazy(() => import("./pages/NotFound"))
+  const Login = React.lazy(() => import("./pages/auth/Login"))
+  const Register = React.lazy(() => import("./pages/auth/Register"))
+  const Forgot = React.lazy(() => import("./pages/auth/Forgot"))
+  const MainLayout = React.lazy(() => import("./layouts/MainLayout"))
+  const AuthLayout = React.lazy(() => import("./layouts/AuthLayout"))
+  const ErrorPage = React.lazy(() => import("./pages/ErrorPage"))
+
   // const [count, setCount] = useState(0);
 
   return (
+<Suspense fallback={<Loading />}>
+
     <Routes>
       <Route element={<MainLayout />}>
         <Route path="/" element={<Dashboard />} />
@@ -64,13 +80,14 @@ function App() {
         {/* HARUS PALING BAWAH */}
         <Route path="*" element={<NotFound />} />
       </Route>
-      
+
       <Route element={<AuthLayout/>}>
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register/>} />
             <Route path="/forgot" element={<Forgot/>} />
         </Route>
     </Routes>
+    </Suspense>
   );
 }
 
--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,43 +1,123 @@
-export default function Login() {
-    return (
-        <div>
-            <h2 className="text-2xl font-semibold text-gray-700 mb-6 text-center">
-                Welcome Back 👋
-            </h2>
+import { FaTruckLoading } from "react-icons/fa";
+import { GiTerror } from "react-icons/gi";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
-            <form>
-                <div className="mb-5">
-                    <label className="block text-sm font-medium text-gray-700 mb-1">
-                        Email Address
-                    </label>
-                    <input
-                        type="text"
-                        id="email"
-                        className="w-full px-4 py-2 bg-gray-50 border border-gray-300 rounded-lg shadow-sm
+export default function Login() {
+  /* navigate, state & handleChange*/
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+  const [dataForm, setDataForm] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (evt) => {
+    const { name, value } = evt.target;
+    setDataForm({
+      ...dataForm,
+      [name]: value,
+    });
+  };
+
+  /* process form */
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    setLoading(true);
+    setError(false);
+
+    axios
+      .post("https://dummyjson.com/user/login", {
+        username: dataForm.email,
+        password: dataForm.password,
+      })
+      .then((response) => {
+        // Jika status bukan 200, tampilkan pesan error
+        if (response.status !== 200) {
+          setError(response.data.message);
+          return;
+        }
+
+        // Redirect ke dashboard jika login sukses
+        navigate("/");
+      })
+      .catch((err) => {
+        if (err.response) {
+          setError(err.response.data.message || "An error occurred");
+        } else {
+          setError(err.message || "An unknown error occurred");
+        }
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
+  /* error & loading status */
+  const errorInfo = error ? (
+    <div className="bg-red-200 mb-5 p-5 text-sm font-light text-gray-600 rounded flex items-center">
+      <GiTerror className="text-red-600 me-2 text-lg" />
+      {error}
+      <FaTruckLoading />
+    </div>
+  ) : null;
+
+  const loadingInfo = loading ? (
+    <div className="bg-gray-200 mb-5 p-5 text-sm rounded flex items-center">
+      <FaTruckLoading className="me-2 animate-spin" />
+      Mohon Tunggu...
+    </div>
+  ) : null;
+
+  return (
+    <div>
+      <h2 className="text-2xl font-semibold text-gray-700 mb-6 text-center">
+        Welcome Back 👋
+      </h2>
+
+      {errorInfo}
+
+      {loadingInfo}
+
+      <form onSubmit={handleSubmit}>
+        <div className="mb-5">
+          <label className="block text-sm font-medium text-gray-700 mb-1">
+            Email Address
+             
+          </label>
+          <input
+            type="text"
+            id="email"
+            className="w-full px-4 py-2 bg-gray-50 border border-gray-300 rounded-lg shadow-sm
                             placeholder-gray-400"
-                        placeholder="you@example.com"
-                    />
-                </div>
-                <div className="mb-6">
-                    <label className="block text-sm font-medium text-gray-700 mb-1">
-                        Password
-                    </label>
-                    <input
-                        type="password"
-                        id="password"
-                        className="w-full px-4 py-2 bg-gray-50 border border-gray-300 rounded-lg shadow-sm
-                            placeholder-gray-400"
-                        placeholder="********"
-                    />
-                </div>
-                <button
-                    type="submit"
-                    className="w-full bg-gray-500 hover:bg-green-600 text-white font-semibold py-2 px-4
-                        rounded-lg transition duration-300"
-                >
-                    Login
-                </button>
-            </form>
+            placeholder="you@example.com"
+            onChange={handleChange}
+          />
         </div>
-    )
+        <div className="mb-6">
+          <label className="block text-sm font-medium text-gray-700 mb-1">
+            Password
+          </label>
+          <input
+            type="password"
+            id="password"
+            className="w-full px-4 py-2 bg-gray-50 border border-gray-300 rounded-lg shadow-sm
+                            placeholder-gray-400"
+            placeholder="********"
+            onChange={handleChange}
+          />
+        </div>
+        <button
+          type="submit"
+          className="w-full bg-gray-500 hover:bg-green-600 text-white font-semibold py-2 px-4
+                        rounded-lg transition duration-300"
+        >
+          Login
+        </button>
+      </form>
+    </div>
+  );
 }
